e8c92c4e8a808112b8fe4f2eef01684c8fab298d
Harun Geckaldi <harungeckaldi@gmail.com>
2025-08-05 18:47:06 +0100

    update-content

--- src/resources/content.js
+++ src/resources/content.js
@@ -11,13 +11,14 @@ const content = {
      description: "Fully customizable design system for Next.js and Figma.", // optional
      url: "https://www.denizco.de/", // required if link object exists
      favicon: false,
-     cover: "/images/cover.jpg"
+     cover: "https://www.denizco.de/_next/image?url=%2Fimages%2Fprojects%2Fproject-01%2Fcover-01.jpg&w=3840&q=75"
     },
     {
       title: "Metadata Solutions", // optional
       description: "A great company for all your digital needs", // optional
       url: "https://metadatasolutions.co.uk", // required if link object exists
-      favicon: false
+      favicon: false,
+      cover: "https://www.denizco.de/_next/image?url=%2Fimages%2Fprojects%2Fproject-01%2Fcover-01.jpg&w=3840&q=75"
     },
     {
       title: "MaJesty PUBG Mobile", // optional
